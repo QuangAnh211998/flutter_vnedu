e69b2cea7431357c40a6a1355929220ef1796dd6
QuangAnh211998 <voquanganh12@gmail.com>
2021-01-21 08:21:20 +0700

    update

--- lib/view/Resetmatkhau.dart
+++ lib/view/Resetmatkhau.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_vnedu/view/dangnhap.dart';
 import 'package:flutter_vnedu/view/CapnhatMK.dart';
@@ -29,51 +30,51 @@ class _State extends State<khoiphucmkPage> {
       ),
       body: Container(
         width: size.width * 0.8,
-        margin: EdgeInsets.fromLTRB(size.width*0.1, size.height*0.03, size.width*0.1, 0),
+        margin: EdgeInsets.fromLTRB(
+            size.width * 0.1, size.height * 0.03, size.width * 0.1, 0),
         child: Column(
-          children: <Widget> [
+          children: <Widget>[
             Row(
-            children: <Widget> [
-              Text('Khôi phục mật khẩu', style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black
-              ),),
-          ],
-        ),
-
-            SizedBox(height: size.height*0.015),
-            Text('Vui lòng nhập email của bạn để tiến hành khôi phục tài khoản:',
-              style: TextStyle(
-                height: 1.5,
-                fontSize: 16.0,
-                color: Colors.black87
-            ),),
-            SizedBox(height: size.height*0.05),
+              children: <Widget>[
+                Text(
+                  'Khôi phục mật khẩu',
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+              ],
+            ),
+            SizedBox(height: size.height * 0.015),
+            Text(
+              'Vui lòng nhập email của bạn để tiến hành khôi phục tài khoản:',
+              style:
+                  TextStyle(height: 1.5, fontSize: 16.0, color: Colors.black87),
+            ),
+            SizedBox(height: size.height * 0.05),
             TextField(
               decoration: InputDecoration(
                   hintText: "example@gmail.com",
-                  prefixIcon: Icon(Icons.person_outline)
-              ),
+                  prefixIcon: Icon(Icons.person_outline)),
             ),
-            SizedBox(height: size.height*0.05),
+            SizedBox(height: size.height * 0.05),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget> [
+              children: <Widget>[
                 InkWell(
                   onTap: openpageHomthu,
                   child: Container(
                     alignment: AlignmentDirectional.center,
-                    width: size.width*0.8,
+                    width: size.width * 0.8,
                     height: 50,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
                       color: Color(0xffBB2634),
                     ),
-                    child: Text('Gửi email', style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.white
-                    ),),
+                    child: Text(
+                      'Gửi email',
+                      style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    ),
                   ),
                 )
               ],
@@ -83,14 +84,14 @@ class _State extends State<khoiphucmkPage> {
       ),
     );
   }
-  void openpageHomthu(){
+
+  void openpageHomthu() {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => homthuPage()),
     );
   }
 }
-
 
 class homthuPage extends StatefulWidget {
   @override
@@ -116,8 +117,9 @@ class _homthuPageState extends State<homthuPage> {
         ),
         body: Container(
           width: size.width * 0.9,
-          margin: EdgeInsets.fromLTRB(size.width*0.05, size.height*0.05, size.width*0.05, 0),
-          child:  Column(
+          margin: EdgeInsets.fromLTRB(
+              size.width * 0.05, size.height * 0.05, size.width * 0.05, 0),
+          child: Column(
             children: <Widget>[
               Container(
                 width: 120,
@@ -129,10 +131,10 @@ class _homthuPageState extends State<homthuPage> {
                 child: Icon(Icons.email_outlined,
                     size: 60, color: Color(0xffBB2634)),
               ),
-              SizedBox(height: size.height*0.03),
+              SizedBox(height: size.height * 0.03),
               Container(
                 alignment: AlignmentDirectional.center,
-                width: size.width*0.65,
+                width: size.width * 0.65,
                 child: Text(
                   'Kiểm tra hòm thư của bạn',
                   style: TextStyle(
@@ -144,14 +146,14 @@ class _homthuPageState extends State<homthuPage> {
               ),
               Container(
                 alignment: AlignmentDirectional.center,
-                width: size.width*0.85,
+                width: size.width * 0.85,
                 height: 60,
                 child: Text(
                     'Chúng tôi đã gửi hưỡng dẫn thay đổi mật khẩu vào email của bạn',
                     style: TextStyle(fontSize: 16.0, color: Colors.black87),
                     textAlign: TextAlign.center),
               ),
-              SizedBox(height: size.height*0.04),
+              SizedBox(height: size.height * 0.04),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
@@ -159,7 +161,7 @@ class _homthuPageState extends State<homthuPage> {
                     onTap: openCapnhatMk,
                     child: Container(
                       alignment: AlignmentDirectional.center,
-                      width: size.width*0.85,
+                      width: size.width * 0.85,
                       height: 50,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
@@ -173,7 +175,7 @@ class _homthuPageState extends State<homthuPage> {
                   )
                 ],
               ),
-              SizedBox(height: size.height*0.015),
+              SizedBox(height: size.height * 0.015),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
@@ -188,63 +190,55 @@ class _homthuPageState extends State<homthuPage> {
               ),
               Expanded(
                   child: Container(
-                    alignment: AlignmentDirectional.center,
-                    width: size.width*0.9,
-                    margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        Text('Không nhận được email? Kiểm tra hòm thư spam',
-                          style: TextStyle(
-                              fontSize: 16.0,
-                              color: Colors.black87
-                          ), textAlign: TextAlign.center,
-                        ),
-                        SizedBox(height: 5),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget> [
-                            Text('của bạn hoặc ',style: TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.black87
-                            )),
-                            InkWell(
-                              onTap: exitKhoiphucMk,
-                              child: Text('Thử lại với email khác',
-                                  style: TextStyle(
-                                      fontSize: 16.0,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color(0xffBB2634)
-                                  )),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  )),
+                alignment: AlignmentDirectional.bottomCenter,
+                width: size.width * 0.9,
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    text:
+                        "Không nhận được email? Kiểm tra hòm thư spam của bạn hoặc",
+                    style: TextStyle(
+                        fontSize: 16, color: Colors.black, height: 1.5),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: "Thử lại với một Email khác.",
+                        recognizer: new TapGestureRecognizer()
+                          ..onTap = () {
+                            exitKhoiphucMk();
+                          },
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xffBB2634),
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                ),
+              )),
             ],
           ),
-        )
-
-    );
+        ));
   }
-  void openCapnhatMk(){
+
+  void openCapnhatMk() {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => capnhatmkPage()),
     );
   }
-  void exitDangnhap(){
+
+  void exitDangnhap() {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => dangnhapPage()),
     );
   }
-  void exitKhoiphucMk(){
+
+  void exitKhoiphucMk() {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => khoiphucmkPage()),
     );
   }
 }
-
